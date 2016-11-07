5e8d91172ad3cf730d38f70ccce6dacd6640a67c
Yonsh Lin <yonsh@live.com>
2016-11-08 01:59:44 +0800

    Add Facebook Share Account Extractor

---
+++ Facebook_Share_Account_Extractor.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        Facebook Share Account Extractor
+// @namespace   https://github.com/Yonsh/yonsh-userscripts
+// @include     https://m.facebook.com/shares/view?id=*
+// @version     1
+// @grant       none
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// ==/UserScript==
+
+var $btn = $('<a href="#">抓取分享帳號</a>');
+$('body').prepend($btn);
+$btn.click(function() {
+  var $table = $('<table border="1"></table>');
+  $names = $('#m_story_permalink_view div div div div h3 strong a');
+  for (var i = 0; i < $names.length; i++) {
+    var $row = $('<tr></tr>');
+    $row.append('<td>' + $($names[i]).html() + '</td>');
+    var s = $($names[i]).attr('href');
+    s = 'http://facebook.com' + s.substring(0, s.indexOf('fref=nf&') - 1);
+    $row.append('<td><a href="' + s + '">' + s + '</a></td>');
+    $table.append($row);
+  }
+  $('body').html($table);
+});
