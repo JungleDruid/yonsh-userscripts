552b8ea1d04d3ab1758dc6ce112df2857a4f2d8c
Yonsh Lin <yonsh@live.com>
2016-11-08 02:09:40 +0800

    Add duplication detector

--- Facebook_Share_Account_Extractor.user.js
+++ Facebook_Share_Account_Extractor.user.js
@@ -2,7 +2,7 @@
 // @name        Facebook Share Account Extractor
 // @namespace   https://github.com/Yonsh/yonsh-userscripts
 // @include     https://m.facebook.com/shares/view?id=*
-// @version     1
+// @version     1.0.0
 // @grant       none
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
@@ -12,13 +12,17 @@ $('body').prepend($btn);
 $btn.click(function() {
   var $table = $('<table border="1"></table>');
   $names = $('#m_story_permalink_view div div div div h3 strong a');
+  var flags = [];
   for (var i = 0; i < $names.length; i++) {
     var $row = $('<tr></tr>');
     $row.append('<td>' + $($names[i]).html() + '</td>');
     var s = $($names[i]).attr('href');
     s = 'http://facebook.com' + s.substring(0, s.indexOf('fref=nf&') - 1);
     $row.append('<td><a href="' + s + '">' + s + '</a></td>');
-    $table.append($row);
+    if (!flags[s]) {
+      flags[s] = true;
+      $table.append($row);
+    }
   }
   $('body').html($table);
 });
